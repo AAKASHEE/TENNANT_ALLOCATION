97303dca8f426ada9917dadb2ea94518e0242f14
AAKASHEE <aakashpatra253@gmail.com>
2025-01-19 03:36:17 +0530

    fixed some bugs

--- src/App.tsx
+++ src/App.tsx
@@ -543,7 +543,7 @@ const PropertyDetail = () => {
             className="block"
           >
             <img
-              src="../../img/IMG_D835C67C3AEF-1.jpeg"
+              src="../../prop_1/img/IMG_D835C67C3AEF-1.jpeg"
               alt="Map Location"
               className="w-full h-auto cursor-pointer"
             />
